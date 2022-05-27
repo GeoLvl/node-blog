365e20fd028a3197ec4251aada0e676dafae4300
geoffrey <laville.geoffrey@gmail.com>
2022-05-27 16:22:12 +0200

    requests & responses part

--- server.js
+++ server.js
@@ -1,7 +1,57 @@
 const http = require('http');
+const fs = require('fs');
 
+
+// REQUEST PART
+// const server = http.createServer((req, res) => {
+//     console.log(req.url, req.method);
+// })
+
+
+//RESPONSE PART
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    // res.write('<p>hello, ninjas</p>');
+    // res.write('<p>hello again, ninjas</p>');
+    // res.end()
+
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+            case '/about-me':
+                res.statusCode = 301;
+                res.setHeader('Location', '/about');
+                res.end();
+                break;
+        default :
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    //send an html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end
+        } else {
+            res.write(data);
+            
+            res.end()
+        }
+    })
+
 })
 
 server.listen(3000, 'localhost', () => {
